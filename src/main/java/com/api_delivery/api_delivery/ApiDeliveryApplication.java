fc3df899703dad96e9598937be2a08b991a093df
Jonnas <jonnas.oliveira@sptech.school>
2024-07-18 12:55:08 -0300

    init commit

---
+++ src/main/java/com/api_delivery/api_delivery/ApiDeliveryApplication.java
@@ -0,0 +1,13 @@
+package com.api_delivery.api_delivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiDeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiDeliveryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api_delivery/api_delivery/ApiDeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api_delivery.api_delivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiDeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
